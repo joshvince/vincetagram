70adb037fa448dfcd6cb1161eba22afde002b3e3
Josh Vince <joshmvince@gmail.com>
2023-07-20 17:02:49 +0100

    Add a user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_06_071623) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_20_155843) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -48,6 +48,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_06_071623) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "seq", default: -> { "nextval('post_seq'::regclass)" }
+  end
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.boolean "admin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
