84f6786f25f5332b230dbbf76312c19302426aaf
Josh Vince <joshmvince@gmail.com>
2023-07-24 14:10:29 +0100

    Update service worker controller to ask for notification permissions

--- app/javascript/controllers/service_worker_controller.js
+++ app/javascript/controllers/service_worker_controller.js
@@ -1,21 +1,79 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
+  static targets = [ 'notificationRequester', 'enableNotificationButton' ];
+
   connect() {
+    this.registerServiceWorker()
+    this.handleDisplayOfNotificationRequester()
+  }
+
+  registerServiceWorker() {
     if (navigator.serviceWorker) {
-      navigator.serviceWorker
+      return navigator.serviceWorker
         .register('/service-worker.js', { scope: '/' })
         .then(() => navigator.serviceWorker.ready)
         .then((registration) => {
-          if ('SyncManager' in window) {
-            registration.sync.register('sync-forms');
-          } else {
-            window.alert('This browser does not support background sync.');
-          }
-        })
-        .then(() =>
-          console.log('[ServiceWorkerController]', 'Service worker registered!')
-        );
+          console.log(
+            '[ServiceWorkerController]',
+            'Service worker registered!'
+          );
+          return registration;
+        });
     }
+  }
+
+  handleDisplayOfNotificationRequester() {
+    const currentPermission = Notification.permission;
+
+    if (currentPermission === 'default') {
+      // Do not show the banner if the user has granted or denied permissions
+      this.notificationRequesterTarget.classList.remove('hidden')
+    }
+  }
+
+  enableNotifications() {
+    this.disableNotificationButton()
+
+    Notification.requestPermission()
+      .then((permission) => {
+        if (permission === 'granted') {
+          this.notificationRequesterTarget.classList.add('hidden')
+          // this.subscribeToPushNotifications();
+        } else if (permission === 'denied') {
+          this.notificationRequesterTarget.classList.add('hidden')
+        } else if (permission === 'default') {
+          this.enableNotificationButton()
+        }
+      })
+      .catch((_err) => {
+        this.enableNotificationButton()
+      });
+  }
+
+  subscribeToPushNotifications() {
+    this.registerServiceWorker()
+      .then((registration) => {
+        return registration.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: window.vapidPublicKey,
+        });
+      })
+      .then((subscription) => {
+        // TODO: send this to the server
+        console.log(subscription);
+      });
+  }
+
+  disableNotificationButton() {
+    this.enableNotificationButtonTarget.disabled = true;
+    this.enableNotificationButtonTarget.classList.add('bg-slate-300');
+    this.enableNotificationButtonTarget.innerHTML = 'Waiting';
+  }
+
+  enableNotificationButton() {
+    this.enableNotificationButtonTarget.disabled = false;
+    this.enableNotificationButtonTarget.classList.remove('bg-slate-300');
+    this.enableNotificationButtonTarget.innerHTML = 'Notify me';
   }
 }
